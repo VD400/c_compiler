24cde698f92d0daac4d0186cb08baf2e1d077d67
VD400 <visheshdubey160@gmail.com>
2026-03-07 19:51:11 +0530

    Added symbol_table

---
+++ symbol_table.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct Symbol{
+    char* name;
+    char* data_type;
+    struct Symbol* next;
+} Symbol;
+
+typedef struct Scope{
+    Symbol* symbols;
+    struct Scope* outer;
+}Scope;
+
+Scope* current_scope = NULL;
+
+void push_scope(){
+    Scope* new_scope = (Scope*)malloc(sizeof(Scope));
+    new_scope->symbols = NULL;
+    new_scope->outer = current_scope;
+    current_scope = new_scope;
+    printf("Symbol Table is entering new scope\n");
+}
+
+void pop_scope(){
+    if(current_scope == NULL) return;
+    Scope* old_scope = current_scope;
+    current_scope = current_scope->outer;
+    Symbol* s = old_scope->symbols;
+    while(s){
+        Symbol* temp = s;
+        s = s->next;
+        free(temp->name);
+        free(temp->data_type);
+        free(temp);
+    }
+    free(old_scope);
+    printf("Symbol Table is exiting scope\n");
+}
+
+void add_symbol(char* name, char* type){
+    Symbol* curr = current_scope->symbols;
+    while(curr){
+        if(strcmp(curr->name, name) == 0){
+            fprintf(stderr, "Semantic Error: Variable '%s' already declared in this scope\n", name);
+            exit(1);
+        }
+        curr = curr->next;
+    }
+    Symbol* s = (Symbol*)malloc(sizeof(Symbol));
+    s->name = strdup(name);
+    s->data_type = strdup(type);
+    s->next = current_scope->symbols;
+    current_scope->symbols = s;
+    printf("Symbol Table Declared '%s' as %s\n", name, type);
+}
+
+Symbol* lookup_symbol(char* name){
+    Scope* curr_scope = current_scope;
+    while(curr_scope != NULL){
+        Symbol* s = curr_scope->symbols;
+        while(s){
+            if(strcmp(s->name, name) == 0) return s;
+            s = s->next;
+        }
+        curr_scope = curr_scope->outer; // If not found in inner scope, look at the parent scope
+    }
+    return NULL;
+}
+
+void print_symbol_table(){
+    printf("\n--- SYMBOL TABLE ---\n");
+    Scope* s = current_scope;
+    int depth = 0;
+    while(s){
+        printf("Scope level %d:\n",depth++);
+        Symbol* sym = s->symbols;
+        if(!sym) printf("  (empty)\n");
+        while(sym){
+            printf("  -> %-10s [%s]\n", sym->name, sym->data_type);
+            sym = sym->next;
+        }
+        s = s->outer;
+    }
+    printf("-----------------------------\n");
+}
